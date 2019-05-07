73ff0b7ae3a0cafb362593d5364309a6bd4b1b08
Arpan Patel <ap.patel1893@gmail.com>
2019-05-07 16:34:53 -0500

    post req users

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,46 +1,19 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
-})
-
-// const tu = new User({
-//     name: 'Karunanidhi',
-//     age: 11
-// })
-
-// tu.save().then(() => {
-//     console.log(tu)
-// }).catch((error) => {
-//     console.log('Error', error)
-// })
-
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
-})
-
-const task = new Task({
-    description: 'Tu raji tha',
-    completed: false
-})
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Error', error)
 })
