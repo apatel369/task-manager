e53c802d3b26ca33041768a3c8594acb1b8bb142
Arpan Patel <ap.patel1893@gmail.com>
2019-05-08 14:27:28 -0500

    secure password

--- src/index.js
+++ src/index.js
@@ -15,3 +15,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+// const bcrypt = require('bcryptjs')
+
+// const myFunction = async () => {
+//     const password = 'Red12345!'
+//     const hashedPassword = await bcrypt.hash(password, 8)
+
+//     console.log(password)
+//     console.log(hashedPassword)
+
+//     const isMatch = await bcrypt.compare('Reerd12345!', hashedPassword)
+//     console.log(isMatch)
+// }
+
+// myFunction()
