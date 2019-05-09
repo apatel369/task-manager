7060307d81267f1a5e27f990cba2e4c70873d9e9
Arpan Patel <ap.patel1893@gmail.com>
2019-05-09 15:51:37 -0500

    authenticate task API endpoints

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,29 +17,31 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
+        const tasks = await Task.find({owner: req.user._id})
         res.send(tasks)
     } catch (e) {
         res.status(500).send(e)
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id})
+
         if (!task) {
             return res.status(404).send()
         }
+
         res.send(task)
     } catch(e) {
         res.status(500).send(e)
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -49,7 +51,7 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
 
         updates.forEach((update) => task[update] = req.body[update])
         await task.save()
@@ -64,9 +66,9 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete( {_id: req.params.id, owner: req.user._id})
 
         if (!task) {
             return res.status(404).send()
