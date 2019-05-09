b140e75b3d3988934eed9d11f2ad0ec7864dc181
Arpan Patel <ap.patel1893@gmail.com>
2019-05-08 20:53:30 -0500

    accept token and get profile

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
+const auth = require('../middleware/auth')
+
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body) 
@@ -24,13 +26,8 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send(e)
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
