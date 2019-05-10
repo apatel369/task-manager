80e190bf9055fc81622919a3fe156896d0136940
Arpan Patel <ap.patel1893@gmail.com>
2019-05-09 20:37:31 -0500

    pagination for tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,6 +17,8 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=false
+// GET /tasks?limit10&skip20
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
 
@@ -27,7 +29,11 @@ router.get('/tasks', auth, async (req, res) => {
     try {
         await req.user.populate({
            path: 'tasks',
-           match
+           match,
+           options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+           }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
