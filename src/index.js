6bf8cdefd909cb98eac1e98bf6dcb5caf05ceea4
Arpan Patel <ap.patel1893@gmail.com>
2019-05-09 22:24:26 -0500

    handle express errors

--- src/index.js
+++ src/index.js
@@ -24,8 +24,11 @@ const upload = multer({
         // cb(undefined, false)
     }
 })
+
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send( { error: error.message })
 })
 
 app.use(express.json())
