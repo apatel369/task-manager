891e75feab4cbdc3b2eb8d88faaea3054f5f510b
Arpan Patel <ap.patel1893@gmail.com>
2019-05-09 22:15:04 -0500

    add file validation

--- src/index.js
+++ src/index.js
@@ -9,7 +9,20 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a word doc'))
+        }
+
+        cb(undefined, true)
+        
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, false)
+    }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
     res.send()
